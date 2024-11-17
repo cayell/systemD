64820f5d4681d6af4633fe90f8bf403cc61fadde
cayell <157846638+cayell@users.noreply.github.com>
2024-11-17 14:56:41 -0300

    Update 1func.js

--- 1func.js
+++ 1func.js
@@ -1,5 +1,6 @@
 var viagem = [];
-var x = 0;
+var veiculo = [];
+//var x = 0;
 
 function resgistroSaida(){
     let cod = document.getElementById('codigo').value;
@@ -14,11 +15,8 @@ function resgistroSaida(){
         alert('Codigo ja existente!');
         return;
     }
-    if(cod == '' || model == '' || moto == '' || dest == '' || dist == ''){
-        alert('Campos vazios!');
-        return;
-    }
-    x++;
+    attlocal(model, dest);
+    //x++;
 
     viagem.push({
         codigo: cod,
@@ -27,16 +25,27 @@ function resgistroSaida(){
         destino: dest,
         distancia: dist,
         horaSaida: horaS,
-        horaChegada: "Em transito"
+        horaChegada: "Em transito",
+        peso: 0
     });
-
+    
     localStorage.setItem('viagem', JSON.stringify(viagem));
+    //localStorage.setItem('x', x);
 
     exibirViagem();
 
     //document.getElementById("saida").innerHTML = JSON.stringify(viagem[x++], null, 2);
     //document.getElementById("saida").innerHTML = "Ultimo Registro: " + hora() + "\nQuantidade de Registros: " + x;
     //+ viagem[viagem.length -1].codigo;
+}
+function attlocal(x, y){
+    for(var i = 0; i < veiculo.length; i++){
+        if(veiculo[i].placa == x){
+            veiculo[i].loc = "A caminho de: " + y; 
+        }
+    }
+    localStorage.setItem('veiculo', JSON.stringify(veiculo));
+    
 }
 function registroChegada(){
     let cod = document.getElementById('codigo1').value;
@@ -56,16 +65,65 @@ function registroChegada(){
         }
     }
     
+}
+function cadastroVeiculo(){
+    let cod = document.getElementById('codigoV').value;
+    let model = document.getElementsByName('modelos')[0].value;
+    let placa = document.getElementById('placa').value;
+
+    verifyCod(cod);
+    if(cod == '' || model == '' || placa == ''){
+        alert('Campos vazios!');
+        return;
+    }
+    veiculo.push({
+        codigo: cod,
+        modelo: model,
+        placa: placa,
+        loc: "Garagem"
+    }) 
+
+    localStorage.setItem('veiculo', JSON.stringify(veiculo));
+    exibirVeiculos();
+}
+function exibirVeiculos(){
+    let tabela = document.getElementById('tabelaV').getElementsByTagName('tbody')[0];
+    tabela.innerHTML = '';
+
+    for (let i = 0; i < veiculo.length; i++) {
+        let linha = tabela.insertRow();
+
+        let codv = linha.insertCell(0);
+        let modelo = linha.insertCell(1);
+        let placa = linha.insertCell(2);
+        let local = linha.insertCell(3);
+
+        //usar getelement e inner para adicionar td oculto(e posivel adicionar botao)
+        codv.innerHTML = veiculo[i].codigo;
+        modelo.innerHTML = veiculo[i].modelo;
+        placa.innerHTML = veiculo[i].placa;
+        local.innerHTML = veiculo[i].loc;
+    }
 
 }
 
 function verifyCod(x){
-    for(var i = 0; i<viagem.length; i++){
-        if(viagem[i].codigo==x){
-            return 1;
+    if(document.getElementById('tabela')){
+        for(var i = 0; i<viagem.length; i++){
+            if(viagem[i].codigo==x){
+                return 1;
+            }
         }
+        return 0;
     }
-    return 0;
+    else{
+        for(var i = 0; i<veiculo.length;i++){
+            if(veiculo[i].codigo == x){
+                return 1;
+            }
+        }
+        return 0;
+    }
 }
 function hora(){
     const agora = new Date();
@@ -84,13 +142,13 @@ function exibirViagem() {
     for (let i = 0; i < viagem.length; i++) {
         let linha = tabela.insertRow();
 
-        let codv = linha.insertCell(0);
+        let cod = linha.insertCell(0);
         let condutor = linha.insertCell(1);
         let dest = linha.insertCell(2);
         let butao = linha.insertCell(3);
 
         //usar getelement e inner para adicionar td oculto(e posivel adicionar botao)
-        codv.innerHTML = viagem[i].codigo;
+        cod.innerHTML = viagem[i].codigo;
         condutor.innerHTML = viagem[i].condutor;
         dest.innerHTML = viagem[i].destino;
         butao.innerHTML = '<button type="button" onclick="mostrarMais(this)">Mostrar Mais</button>'
@@ -124,12 +182,21 @@ function mostrarMais(btn) {
 }
 
 function carregarDados() {
-    let dadosSalvos = localStorage.getItem('viagem'); 
-    if (dadosSalvos != null) {
-        viagem = JSON.parse(dadosSalvos);
+    let dadosViagem = localStorage.getItem('viagem'); 
+    let dadosVeiculo = localStorage.getItem('veiculo');
+    if (dadosViagem != null && document.getElementById('tabela')) {
+        viagem = JSON.parse(dadosViagem);
+        veiculo = JSON.parse(dadosVeiculo);
         console.log(viagem.length);
+        console.log(veiculo);
         exibirViagem();
     }
+    else if(dadosVeiculo != null && document.getElementById('tabelaV')){
+        veiculo = JSON.parse(dadosVeiculo);
+        viagem = JSON.parse(dadosViagem);
+        exibirVeiculos();
+    }
+    //x = localStorage.getItem('x');
 }
 
 document.addEventListener('DOMContentLoaded', carregarDados);
